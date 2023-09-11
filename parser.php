2f8a9c5310afa323d1dd6f1fa999f2e9992c304b
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-09-11 13:55:14 -0500

    MVP

--- parser.php
+++ parser.php
@@ -1,17 +1,59 @@
-<body style = "line-height: 1.5rem;font-family:monospace; font-size:1rem;">
+<body>
 <?php
-    $FEED = "https://careers.brandtholdings.com/feed/313300";
-    $xml = simplexml_load_file($FEED);
-    $description = $xml -> jobs -> job[0] -> description;
-    //replace all double quotes with single quotes- will not output HTML otherwise
-    $description = str_replace('"', '\'', $description);
-    $description = str_replace('“', '\'', $description);
-    $description = str_replace('”', '\'', $description);
-    $dom = new DOMDocument('1.0', 'utf-8');
-    $dom -> loadHTML($description, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-    $description = str_replace('"', '\'', $dom -> saveHTML());
-    echo htmlspecialchars_decode($description);
+     $FEED = "https://careers.brandtholdings.com/feed/313300";
+     $xml = simplexml_load_file($FEED);
 
+    function parseDescription($job)
+    {
+        $description = $job -> description;
+        //replace all double quotes with single quotes- will not output HTML otherwise
+        $description = str_replace('"', '\'', $description);
+        $description = str_replace('“', '\'', $description);
+        $description = str_replace('”', '\'', $description);
+        $dom = new DOMDocument('1.0', 'utf-8');
+        $dom -> loadHTML($description, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        $description = str_replace('"', '\'', $dom -> saveHTML());
+        return htmlspecialchars_decode($description);
+    }
+
+    function getURLParams($job)
+    {
+        $url = $job -> url;
+        $url = parse_url($url);
+        parse_str($url['query'], $query);
+        return $query;
+    }
+
+    function jobsToTable($jobs){
+        $table = "<table>";
+        $table .= "<tr>";
+        $table .= "<th>Job Title</th>";
+        $table .= "<th>Location</th>";
+        $table .= "<th>Reference Number</th>";
+        $table .= "<th>Business Unit</th>";
+        $table .= "<th>Facility</th>";
+        $table .= "<th>Date</th>";
+        $table .= "<th>URL</th>";
+        $table .= "</tr>";
+        foreach($jobs as $job){
+            $table .= "<tr>";
+            $table .= "<td><details style = 'display:inline'>
+            <summary></summary>
+            <p>" . parseDescription($job) . "</p>
+            </details>" . $job -> title . "</td>";
+            $table .= "<td>" . $job -> location . "</td>";
+            $table .= "<td>" . $job -> referencenumber . "</td>";
+            $table .= "<td>" . $job -> business_unit . "</td>";
+            $table .= "<td>" . $job -> facility . "</td>";
+            $table .= "<td>" . $job -> date . "</td>";
+            $table .= "<td><a href =" . $job -> url . ">View</a></td>";
+            $table .= "</tr>";
+        }
+        $table .= "</table>";
+        return $table;
+    }
+
+    echo jobsToTable($xml -> jobs -> job);
 
 ?>
 </body>
